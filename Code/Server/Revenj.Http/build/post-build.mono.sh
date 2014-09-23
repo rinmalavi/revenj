d7aec06bf69d795a42cc70325ca9569ef781ccc7
rinmalavi@gmail.com <rinmalavi@gmail.com>
2014-09-23 19:33:58 +0200

    random changes

--- Code/Server/Revenj.Http/build/post-build.mono.sh
+++ Code/Server/Revenj.Http/build/post-build.mono.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 exec cp \
-	../../../Features/NGS.Features.Storage/bin/Mono/NGS.Features.Storage.dll* \
-	../../../Features/NGS.Features.Mailer/bin/Debug/NGS.Features.Mailer.dll* \
+	../../../Features/Revenj.Features.Storage/bin/Mono/Revenj.Features.Storage.dll* \
+	../../../Features/Revenj.Features.Mailer/bin/Debug/Revenj.Features.Mailer.dll* \
 	../../../Features/Revenj.Features.RestCache/bin/Release/Revenj.Features.RestCache.dll* \
-	../../../Plugins/NGS.Plugins.DatabasePersistence.Postgres/bin/Mono/NGS.Plugins.DatabasePersistence.Postgres.dll* \
+	../../../Plugins/Revenj.Plugins.DatabasePersistence.Postgres/bin/Mono/Revenj.Plugins.DatabasePersistence.Postgres.dll* \
 	../../../Plugins/Revenj.Plugins.Rest.Commands/bin/Mono/Revenj.Plugins.Rest.Commands.dll* \
 	../../../Plugins/Revenj.Plugins.Server.Commands/bin/Mono/Revenj.Plugins.Server.Commands.dll* \
 	../bin/Debug/
